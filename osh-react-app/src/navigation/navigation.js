3b8dd07a470d744a00a2fd122f5b1aa108843928
Esi77t <2089651@naver.com>
2025-05-07 10:56:41 +0900

    commit

--- osh-react-app/src/navigation/navigation.js
+++ osh-react-app/src/navigation/navigation.js
@@ -1,17 +1,18 @@
 import { useContext } from "react";
 import { ThemeContext } from "../context/context";
 import { Link } from "react-router-dom";
+import "../index.css"
 
 export const NavBar = () => {
 
     const { theme } = useContext(ThemeContext);
 
     return(
-        <div className={`navbar ${ theme }`}>
+        <nav className={`navbar ${ theme }`}>
             <Link to="/">Home</Link>
             <Link to="/posts">Posts</Link>
             <Link to="/settings">Settings</Link>
-        </div>
+        </nav>
     )
 
 }
